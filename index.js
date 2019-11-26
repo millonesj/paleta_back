0e9d78c190eda89260059a49e4a7a3dbc0ced0ce
Jhonny Neciosup Millones <jhonny.millones@gmail.com>
2019-11-25 20:47:56 -0500

    feat(logging): Add logger

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const logger = require('./src/libs/logger');
 
 /* configuration */
 const config = {
@@ -23,6 +24,12 @@ mongoose.connection.on('error', (error) => {
   logger.error('Failed to connect to database', error);
   process.exit(1);
 });
+
+/* MIDDLEWARES */
+app.use(express.json());
+app.use(morgan('{"remote_addr": ":remote-addr", "remote_user": ":remote-user", "date": ":date[clf]", "method": ":method", "url": ":url", "http_version": ":http-version", "status": ":status", "result_length": ":res[content-length]", "referrer": ":referrer", "user_agent": ":user-agent", "response_time": ":response-time"}', {
+  stream: logger.stream}));
+app.use((req,res,next) => logger.saveParams(req,res,next));
 
 app.listen(config.PORT, () => {
   console.log(`We app is listening on port: ${config.PORT}`);
---
+++ src/libs/logger.js
@@ -0,0 +1,46 @@
+const { createLogger, format, transports } = require('winston');
+const logger = createLogger({
+
+  format: format.json(),
+  /* defaultMeta: {service: 'shop-api'}, */
+  transports: [
+    new transports.File({
+      level: 'info',
+      format: format.combine(
+        format.timestamp({
+          format: 'YYYY-MM-DD HH:mm:ss'
+      }),
+        format.json()
+      ),
+      exitOnError: false,
+      maxSize: 512000,
+      maxFiles: 5,
+      filename: `${__dirname}/log-de-aplicacion.log`,
+      prettyPrint: object => { return JSON.stringify(object) }
+    }),
+    new transports.Console({
+      level: 'debug',
+      exitOnError: false,
+      format: format.combine(
+        format.colorize(),
+        format.simple()),
+    })
+  ]
+})
+
+// logging morgan info
+logger.stream = {
+  write: function(message, encoding){
+    logger.info('morgan',{request: message.trim()});
+  }
+}
+
+// logging params request
+logger.saveParams = (req, rest, next ) => {
+  let params = req.body;
+  if(req.body.password) params = {...params, password:'***'}
+  logger.info('params',{params,url:req.url});
+  next();
+}
+
+module.exports =  logger;
