304856f83ef85913265caf570ff6dff5eca3dbbb
Jhonny Neciosup Millones <jhonny.millones@gmail.com>
2019-12-09 01:28:54 -0500

    refactor(chat): Save chats

--- src/chat-server/index.js
+++ src/chat-server/index.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const jwt = require('jsonwebtoken');
+const axios = require('axios');
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>');
@@ -13,10 +15,50 @@ http.listen(PORT, () => {
   console.log(`Listening on *:${PORT}`);
 });
 
-io.on('connection', function(socket) {
+io.use(function(socket, next) {
+  if (socket.handshake.query && socket.handshake.query.token) {
+    jwt.verify(socket.handshake.query.token, 'SECRET_KEY', function(
+      err,
+      decoded
+    ) {
+      if (err) return next(new Error('Authentication error'));
+      socket.decoded = decoded;
+      socket.token = socket.handshake.query.token;
+      next();
+    });
+  } else {
+    next(new Error('Authentication error'));
+  }
+}).on('connection', function(socket) {
   console.log('a user connected');
   socket.on('chat message', function(msg) {
     console.log('message: ' + JSON.stringify(msg));
+    const token = socket.token;
+    const chat = {
+      proyectId: msg.proyectId,
+      message: msg.msg,
+      userId: msg.from
+    };
+    saveChat(token, chat);
     io.emit('chat message', msg);
   });
 });
+
+const saveChat = (token, chat) => {
+  var config = {
+    headers: { Authorization: 'bearer ' + token }
+  };
+
+  var bodyParameters = {
+    ...chat
+  };
+
+  axios
+    .post('http://localhost:3002/chats', bodyParameters, config)
+    .then(response => {
+      //console.log(response);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+};
